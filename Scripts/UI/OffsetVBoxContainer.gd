2fdaf4d43ae2a8d14a9c7300ac41eb4704111f86
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-05-12 10:07:16 -0500

    h75.3 - Better Main Menu Buttons
    Made the main menu buttons bump out a bit when selected, among other things.

---
+++ Scripts/UI/OffsetVBoxContainer.gd
@@ -0,0 +1,34 @@
+@tool
+class_name OffsetVBoxContainer extends VBoxContainer
+
+@export var offset := 30.0
+
+func _process(_delta: float) -> void:
+	
+	var best := _best_option()
+	
+	for child in get_children(): if child is Control:
+		child.position.x = offset if best == child else 0.0
+	
+	if best: if not best.has_focus(): best.grab_focus()
+
+func _best_option() -> Control:
+	
+	var best:Control = null
+	for child in get_children(): if child is Control:
+		var selection_rank := _selected(child)
+		
+		## If it's focused, it'll be chosen if nothing is hovered.
+		if selection_rank == 1 and best == null:
+			best = child
+		## If it's hovered, it's chosen.
+		elif selection_rank == 2: return child
+	
+	return best
+
+func _selected(child:Control) -> int:
+	if child.has_focus(): return 1
+	
+	if child is BaseButton: if child.is_hovered(): return 2
+	
+	return 0
