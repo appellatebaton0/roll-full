a4400613bb3e8e8a55cdc11be22012456cfd0a61
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-05-15 12:28:00 -0500

    h83.8 - Options Screen Styling + Polish
    BIG commit; The entire options screen is now very fancily styled, and should be just about done. It's kinda lacking in settings, but I can't think of any others it needs.

--- Scripts/UI/OptionsScreen.gd
+++ Scripts/UI/OptionsScreen.gd
@@ -1,12 +1,14 @@
-class_name OptionsScreen extends Panel
+class_name OptionsScreen extends Control
 ## Manages some of the happenings in the options screen.
 
 ## All the configurable inputs. Input Name : Display Name
 @export var reconfigurable_inputs:Dictionary[StringName, String]
 
-@onready var remap_box := $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/ControlsBox/RemapBox
+@onready var remap_box := $MarginContainer/VBoxContainer/PanelContainer/PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/ControlsBox/PanelContainer/MarginContainer2/RemapBox
 
 const RemapEntryScene := preload("res://Scenes/UIElements/RemapEntry.tscn")
+
+@onready var scroll_cont := $MarginContainer/VBoxContainer/PanelContainer/PanelContainer/MarginContainer/ScrollContainer
 
 func _ready() -> void:
 	
@@ -16,12 +18,25 @@ func _ready() -> void:
 		new.action = input
 		new.label_override = reconfigurable_inputs[input]
 		
+		new.focus_neighbor_top = remap_box.get_node(remap_box.focus_neighbor_top).get_path()
+		
 		remap_box.add_child(new)
 	
-	focus_entered.connect(_on_focused)
+	var scroll_focusees := scroll_cont.get_children()
+	while len(scroll_focusees) > 0:
+		var this:Node = scroll_focusees.pop_back()
+		
+		if this is Control: if this.focus_mode == FOCUS_ALL:
+			this.focus_entered.connect(_on_scroll_focus.bind(this))
+		
+		scroll_focusees += this.get_children()
 	
+	focus_entered.connect(_on_focused)
 
-func _on_focused() -> void: $MarginContainer/VBoxContainer/Title/Button.grab_focus()
+func _on_focused() -> void: $MarginContainer/VBoxContainer/VBoxContainer/Title/Button.grab_focus()
+
+func _on_scroll_focus(node:Control) -> void:
+	scroll_cont.scroll_vertical -= scroll_cont.global_position.y - node.global_position.y + (scroll_cont.size.y / 2)
 
 func _process(delta: float) -> void:
 	if is_visible_in_tree() and Input.is_action_just_pressed("ui_cancel"):
@@ -31,15 +46,3 @@ func _process(delta: float) -> void:
 func close() -> void:
 	Global.request_animation.emit("CloseOptions")
 	if Global.return_focus: Global.return_focus.grab_focus()
-
-
-func _on_remap_box_focused() -> void:
-	var children := remap_box.get_children()
-	while len(children):
-		for child in children:
-			if child is RemapButton:
-				child.grab_focus()
-				return
-		
-		for i in range(len(children)):
-			children += children.pop_front().get_children()
